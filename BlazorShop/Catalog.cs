5bbb5dce9c8ef7836dfa9295f5726054118f69fc
YuryShulga <93866540+YuryShulga@users.noreply.github.com>
2023-12-17 21:53:53 +0300

    Add project files.

---
+++ BlazorShop/Catalog.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorShop
+{
+    public class Catalog
+    {
+        private readonly List<Product> products = new()
+        {
+            new Product{Id = 1, Name = "Clean Code" },
+            new Product{Id = 2, Name = "Elegant objects" }
+        };
+        public IReadOnlyCollection<Product> GetProducts() { 
+            return products;
+        }
+    }
+    public class Product 
+    { 
+        public int Id { get; set;}
+        public string Name { get; set;}
+
+        public decimal Price { get; set;}
+    }
+}
